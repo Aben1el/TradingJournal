217ba7b013e4151ff63d914110298bcd691b597a
Aben1el <Abenezerfekadu07@gmail.com>
2026-09-01 21:27:41 +0300

    Update conn.js

--- js/conn.js
+++ js/conn.js
@@ -43,7 +43,7 @@ void Sync() {
          + "\\"closed_at\\":" + IntegerToString((long)HistoryDealGetInteger(t, DEAL_TIME)) + "}";
       uchar data[]; StringToCharArray(body, data, 0, StringLen(body), CP_UTF8);
       string headers = "apikey: " + TV_Key + "\\r\\nContent-Type: application/json\\r\\nPrefer: return=minimal\\r\\n";
-      char res[]; string h;
+      uchar res[]; string h;
       int code = WebRequest(TV_Url, headers, 15000, data, res, h);
       if (code == 201 || code == 200) from = (datetime)HistoryDealGetInteger(t, DEAL_TIME) + 1;
    }
