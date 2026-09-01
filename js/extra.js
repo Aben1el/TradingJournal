cf5c877d80f43f81d30d413523326734e80f8850
Aben1el <Abenezerfekadu07@gmail.com>
2026-09-01 13:50:48 +0300

    Update extra.js

--- js/extra.js
+++ js/extra.js
@@ -71,7 +71,7 @@ void Sync() {
       char data[]; StringToCharArray(body, data, 0, StringLen(body), CP_UTF8);
       string headers = "apikey: " + TV_Key + "\\r\\nContent-Type: application/json\\r\\nPrefer: return=minimal\\r\\n";
       char res[]; string h;
-      int code = WebRequest("POST", TV_Url, headers, 15000, data, res, h);
+      int code = WebRequest(TV_Url, headers, 15000, data, res, h);
       if (code == 201 || code == 200) from = (datetime)HistoryDealGetInteger(t, DEAL_TIME);
    }
    from = TimeCurrent() - 3600;
